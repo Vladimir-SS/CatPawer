d07308c5597a2f6402740ae77fecf8bbbd7b0c1b
Ana Andrișan <ana.andrisan3@gmail.com>
2023-04-29 10:22:08 +0300

    before i mangle aything

--- GameDesign/Assets/Project/Scripts/MapGen/WallPlacer.cs
+++ GameDesign/Assets/Project/Scripts/MapGen/WallPlacer.cs
@@ -13,10 +13,10 @@ class RoomAttributes
     public Vector3 topRight = new Vector3();
     public Vector3 bottomRight = new Vector3();
 
-    public bool topLeftCovered = false;
-    public bool bottomLeftCovered = false;
-    public bool topRightCovered = false;
-    public bool bottomRightCovered = false;
+    public bool isTopLeftCovered = false;
+    public bool isBottomLeftCovered = false;
+    public bool isTopRightCovered = false;
+    public bool isBottomRightCovered = false;
 
     public RoomAttributes(GameObject room, MapGenerator mapGenerator)
     {
@@ -71,10 +71,7 @@ public class WallPlacer : MonoBehaviour
         this.wall = baseWall;
         this.wallHeightOffset = wallHeightOffset;
 
-        GameObject w = Instantiate(wall);
-        wallHeight = w.GetComponent<MeshRenderer>().bounds.size.y;
-        wallWidth = w.GetComponent<MeshRenderer>().bounds.size.x;
-        Destroy(w);
+        SetWallSizes();
 
         mapGenerator = this.GetComponent<MapGenerator>();
 
@@ -82,21 +79,179 @@ public class WallPlacer : MonoBehaviour
         {
             if (rooms[i] == null)
                 continue;
-            Vector3[] corners = rooms[i].GetComponent<RoomParameters>().Corners;
-            //for (int j = 0; j < corners.Length; j++)
-            //{
-            //    GameObject emptyGameObject = new GameObject(rooms[i].name);
-            //    emptyGameObject.transform.position = corners[j];
-            //}
             roomAttributes.Add(new RoomAttributes(rooms[i], mapGenerator));
         }
 
         SortRooms();
 
+        //foreach(RoomAttributes roomAttribute in roomAttributes)
+        //{
+        //    GameObject emptyGameObject = new GameObject("TR");
+        //    emptyGameObject.transform.position = roomAttribute.topRight;
+
+        //    GameObject emptyGameObject2 = new GameObject("TL");
+        //    emptyGameObject2.transform.position = roomAttribute.topLeft;
+
+        //    GameObject emptyGameObject3 = new GameObject("BR");
+        //    emptyGameObject3.transform.position = roomAttribute.bottomRight;
+
+        //    GameObject emptyGameObject4 = new GameObject("BL");
+        //    emptyGameObject4.transform.position = roomAttribute.bottomLeft;
+
+        //}
+
         foreach(RoomAttributes room in roomAttributes)
         {
             PlaceCorners(room);
-            //PlaceFillerWalls();
+
+            PlaceWallsAlongXAxis(room.topLeft, room.topRight);
+                PlaceWallsAlongXAxis(room.bottomLeft, room.bottomRight);
+        }
+        // ClearDuplicateWalls();
+    }
+
+    private void PlaceWallsAlongXAxis(Vector3 c1, Vector3 c2)
+    {
+        PlaceWallsBetweenPoints(c1, c2);
+        Vector3 leftmost = new Vector3();
+        Vector3 rightmost = new Vector3();
+        if(c1.x < c2.x)
+        {
+            leftmost = c1;
+            rightmost = c2;
+        }
+        else
+        {
+            leftmost = c2;
+            rightmost = c1;
+        }
+        //Vector3 followingPoint = GetFollowingPointToTheRight(rightmost);
+
+        //if (IntersectsAnyCorner(rightmost))
+        //{
+        //    print("right");
+            
+        //    SetIntersectedCornerCovered(rightmost);            
+            
+
+        //    PlaceWallsAlongXAxis(followingPoint, rightmost);
+        //}
+
+        Vector3 followingPoint = GetFollowingPointToTheLeft(leftmost);
+        if (IntersectsAnyCorner(leftmost))
+        {
+            print("left");
+            
+            SetIntersectedCornerCovered(leftmost);
+
+            PlaceWallsAlongXAxis(followingPoint, leftmost);
+        }
+    }
+
+    private Vector3 GetFollowingPointToTheRight(Vector3 point)
+    {
+        Vector3 closestPoint = new Vector3(Mathf.Infinity, Mathf.Infinity, Mathf.Infinity);
+        foreach (RoomAttributes room in roomAttributes)
+        {
+            if (Vector3.Distance(room.topRight, point) < Vector3.Distance(closestPoint, point))
+            {
+                closestPoint = room.topRight;
+            }
+            
+            if (Vector3.Distance(room.bottomRight, point) < Vector3.Distance(closestPoint, point))
+            {
+                closestPoint = room.bottomRight;
+            }
+
+        }
+        return closestPoint;
+    }
+
+    private Vector3 GetFollowingPointToTheLeft(Vector3 point)
+    {
+        Vector3 closestPoint = new Vector3(Mathf.Infinity, Mathf.Infinity, Mathf.Infinity); 
+        foreach (RoomAttributes room in roomAttributes)
+        {
+
+            if (Vector3.Distance(room.topLeft, point) < Vector3.Distance(closestPoint, point))
+            {
+                closestPoint = room.topLeft;
+            }
+
+            if (Vector3.Distance(room.bottomLeft, point) < Vector3.Distance(closestPoint, point))
+            {
+                closestPoint = room.bottomLeft;
+            }
+        }
+        return closestPoint;
+    }
+
+    private void SetIntersectedCornerCovered(Vector3 point)
+    {
+        foreach(RoomAttributes room in roomAttributes)
+        {
+            if (DistanceXZ(point, room.topLeft) <= wallWidth)
+                room.isTopLeftCovered = true;
+            else if (DistanceXZ(point, room.topRight) <= wallWidth)
+                room.isTopRightCovered = true;
+            else if (DistanceXZ(point, room.bottomLeft) <= wallWidth)
+                room.isBottomLeftCovered = true;
+            else if (DistanceXZ(point, room.bottomRight) <= wallWidth)
+                room.isBottomRightCovered = true;
+        }
+    }
+
+    private float DistanceXZ(Vector3 point1, Vector3 point2)
+    {
+        return Mathf.Sqrt(Mathf.Pow(point1.x - point2.x, 2) + Mathf.Pow(point1.z - point2.z, 2));
+    }
+
+    private bool IntersectsAnyCorner(Vector3 point)
+    {
+        foreach(RoomAttributes room in roomAttributes)
+        {
+            if ((DistanceXZ(point, room.topLeft) <= wallWidth) && !room.isTopLeftCovered)
+                return true;
+            if((DistanceXZ(point, room.bottomLeft) <= wallWidth) && !room.isBottomLeftCovered)
+                return true;
+            if((DistanceXZ(point, room.topRight) <= wallWidth) && !room.isTopRightCovered)
+                return true;
+            if((DistanceXZ(point, room.bottomRight) <= wallWidth) && !room.isBottomRightCovered)
+                return true;
+
+        }
+        return false;
+    }
+
+    private void PlaceWallsBetweenPoints(Vector3 p1, Vector3 p2)
+    {
+        Vector3 start = new Vector3();
+        Vector3 end = new Vector3();
+        Vector3 increment = new Vector3();
+        Vector3 currentPosition = new Vector3();
+        if((p1.x < p2.x) || (p1.z < p2.z))
+        {
+            start = p1;
+            end = p2;
+        }
+        else
+        {
+            start = p1;
+            end = p2;
+        }
+
+        if (p1.x < p2.x)
+            increment = new Vector3(1, 0, 0) * wallWidth;
+        else
+            increment = new Vector3(0, 0, 1) * wallWidth;
+
+        currentPosition = start + increment;
+
+        while (currentPosition.x < end.x || currentPosition.z < end.z)
+        {
+            GameObject wall = Instantiate(this.wall, currentPosition, Quaternion.identity);
+            placedWalls.Add(wall);
+            currentPosition += increment;
         }
     }
 
@@ -112,20 +267,82 @@ public class WallPlacer : MonoBehaviour
 
     private void PlaceCorners(RoomAttributes room)
     {
-        // place corner 1
-        GameObject corner1 = Instantiate(wall, room.bottomRight, Quaternion.identity);
+        if(!room.isTopLeftCovered)
+        {
+            GameObject corner = Instantiate(wall, room.topLeft, Quaternion.identity);
+            placedWalls.Add(corner);
+            room.isTopLeftCovered = true;
+        }
+        if (!room.isTopRightCovered) 
+        { 
+            GameObject corner = Instantiate(wall, room.topRight, Quaternion.identity);
+            placedWalls.Add(corner);
+            room.isTopRightCovered = true;
+        }
+        if(!room.isBottomLeftCovered)
+        {
+            GameObject corner = Instantiate(wall, room.bottomLeft, Quaternion.identity);
+            placedWalls.Add(corner);
+            room.isBottomLeftCovered = true;
+        }
+        if(!room.isBottomRightCovered)
+        {
+            GameObject corner = Instantiate(wall, room.bottomRight, Quaternion.identity);
+            placedWalls.Add(corner);
+            room.isBottomRightCovered = true;
+        }
+        //GameObject corner1 = Instantiate(wall, room.bottomRight, Quaternion.identity);
+        //GameObject corner2 = Instantiate(wall, room.topRight, Quaternion.identity);
+        //GameObject corner3 = Instantiate(wall, room.topLeft, Quaternion.identity);
+        //GameObject corner4 = Instantiate(wall, room.bottomLeft, Quaternion.identity);
 
+        //placedWalls.Add(corner1);
+        //placedWalls.Add(corner2);
+        //placedWalls.Add(corner3);
+        //placedWalls.Add(corner4);
+
+        room.isTopRightCovered = true;
+        room.isBottomLeftCovered = true;
+        room.isBottomRightCovered = true;
         //GameObject emptyGameObject = new GameObject("EmptyGameObject");
         //emptyGameObject.transform.position = room.topLeft;
-        placedWalls.Add(corner1);
-        // place corner 2
-        GameObject corner2 = Instantiate(wall, room.topRight, Quaternion.identity);
-        placedWalls.Add(corner2);
-        // place corner 3
-        GameObject corner3 = Instantiate(wall, room.topLeft, Quaternion.identity);
-        placedWalls.Add(corner3);
-        // place corner 4
-        GameObject corner4 = Instantiate(wall, room.bottomLeft, Quaternion.identity);
-        placedWalls.Add(corner4);
+    }
+
+    private void SetWallSizes()
+    {
+        GameObject w = Instantiate(wall);
+
+        MeshRenderer[] meshRenderers = w.GetComponentsInChildren<MeshRenderer>();
+
+        Bounds bounds = meshRenderers[0].bounds;
+
+        for (int i = 1; i < meshRenderers.Length; i++)
+        {
+            bounds.Encapsulate(meshRenderers[i].bounds);
+        }
+
+        wallHeight = bounds.size.y;
+        wallWidth = bounds.size.x;
+        Destroy(w);
+    }
+
+    private void ClearDuplicateWalls()
+    {
+        for(int i = 0; i < placedWalls.Count; i++)
+        {
+            for(int j = i + 1; j < placedWalls.Count; j++)
+            {
+                //if (placedWalls[i].transform.position.x == placedWalls[j].transform.position.x)
+                if (Mathf.Abs(placedWalls[i].transform.position.x - placedWalls[j].transform.position.x) < wallWidth / 2)
+                {
+                    if (Mathf.Abs(placedWalls[i].transform.position.z - placedWalls[j].transform.position.z) < wallWidth / 2)
+                    {
+                        Destroy(placedWalls[j]);
+                        placedWalls.RemoveAt(j);
+                        j--;
+                    }
+                }
+            }
+        }
     }
 }
